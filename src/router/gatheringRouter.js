aa7a55f79e97cb891db2a8b7d4d1ed71d5386e2c
01youngil <cka1782@gmail.com>
2025-04-15 15:08:22 +0900

    gatheringMain

---
+++ src/router/gatheringRouter.js
@@ -0,0 +1,34 @@
+import GatheringHome from "@/views/gathering/GatheringHome.vue";
+import GatheringMain from "@/views/gathering/GatheringMain.vue";
+import MyGathering from "@/views/gathering/MyGathering.vue";
+import RecommendGathering from "@/views/gathering/RecommendGathering.vue";
+import RecommendMeeting from "@/views/gathering/RecommendMeeting.vue";
+
+export const gatheringRouter= [
+    {
+        path:'/silverpotion/gathering/main',
+        name:'GatheringMain',
+        component: GatheringMain
+    },
+    {
+        path:'/silverpotion/gathering/home/:gatheringId',
+        name: 'GatheringHome',
+        component: GatheringHome
+    },
+    {
+        path:'/silverpotion/gathering/mygathering',
+        name: 'MyGathering',
+        component: MyGathering
+    },
+    {
+        path:'/silverpotion/gathering/recommendGathering',
+        name: 'RecommendGathering',
+        component: RecommendGathering
+    },
+    {
+        path:'/silverpotion/gathering/recommendMeeting',
+        name: 'RecommendMeeting',
+        component: RecommendMeeting
+    },
+    
+]
---
+++ src/views/gathering/MyGathering.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>내 모임</h1>
+</template>
+
+<script>
+export default{
+    data(){
+        return {
+            
+        }
+    },
+}
+</script>
