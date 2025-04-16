1ccb584e9b3711073aa45f850b300d5a49b77471
01youngil <cka1782@gmail.com>
2025-04-16 21:13:18 +0900

    feat/gathering

--- src/views/gathering/MyGathering.vue
+++ src/views/gathering/MyGathering.vue
@@ -1,5 +1,18 @@
 <template>
-    <h1>내 모임</h1>
+    <v-container>
+        <v-card flat class="primary fixed-header" color="primary">
+            <v-card-text class="d-flex align-center pa-2">
+                <v-btn icon @click="handleBackButton" class="mr-2" flat>
+                    <v-icon>mdi-chevron-left</v-icon>
+                </v-btn>
+                <h1 class="text-h5 font-weight-bold my-2 text-center flex-grow-1 text-white">내 모임</h1>
+            </v-card-text>
+        </v-card>
+
+        <div class="content-wrapper">
+            <!-- 기존 내용 -->
+        </div>
+    </v-container>
 </template>
 
 <script>
@@ -9,5 +22,28 @@ export default{
             
         }
     },
+    methods: {
+        handleBackButton() {
+            this.$router.go(-1);
+        }
+    }
 }
 </script>
+
+<style scoped>
+.fixed-header {
+    position: fixed;
+    top: 56px; /* 기존 헤더 컴포넌트 높이에 맞게 조정 */
+    left: 0;
+    right: 0;
+    z-index: 999;
+    max-width: 768px; /* 앱 컨테이너 크기에 맞게 조정 */
+    margin: 0 auto;
+    width: 100%;
+}
+
+.content-wrapper {
+    margin-top: 110px; /* 헤더 높이 + 기존 헤더 컴포넌트 높이에 맞게 조정 */
+    padding: 0 16px;
+}
+</style>
